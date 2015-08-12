f49adc445ae0824c6b5c9fce7eabb8ef1feb43ae
GuyAllard <guyallard01@gmail.com>
2015-08-12 10:33:51 +0200

    changed variable names for clarity

--- Templates/Empty/game/shaders/common/triplanar/hlsl/triplanarCommon.hlsl
+++ Templates/Empty/game/shaders/common/triplanar/hlsl/triplanarCommon.hlsl
@@ -2,29 +2,29 @@
 #define _TRIPLANAR_COMMON_HLSL_
 
 // triplanar UV cordinates from world space position and world space normal
-void worldSpaceTriplanarUVs(float3 wsPosition, float3 wsNormal, out float3 tri_u, out float3 tri_v)
+void worldSpaceTriplanarUVs(float3 position, float3 normal, out float3 u, out float3 v)
 {
     float2 uvX;
     float2 uvY;
     float2 uvZ;
     
-    uvX = wsPosition.yz;
-    uvY = wsPosition.xz;
-    uvZ = wsPosition.xy;
+    uvX = position.yz;
+    uvY = position.xz;
+    uvZ = position.xy;
     
     // rotate uv coords around +z axis
-    uvX.x *= sign(wsNormal.x);
-    uvY.x *= -sign(wsNormal.y);
+    uvX.x *= sign(normal.x);
+    uvY.x *= -sign(normal.y);
     
-    tri_u = float3(uvX.x, uvY.x, uvZ.x);
-    tri_v = float3(uvX.y, uvY.y, uvZ.y);
+    u = float3(uvX.x, uvY.x, uvZ.x);
+    v = float3(uvX.y, uvY.y, uvZ.y);
  }
 
 // triplanar blending weights from world space normal
-float3 triBlendWeights(float3 wsNormal, float tightness)
+float3 triBlendWeights(float3 normal, float tightness)
 {
     //float3 weights = wsNormal*wsNormal;
-    float3 weights = pow(abs(wsNormal), tightness);
+    float3 weights = pow(abs(normal), tightness);
     weights = max(weights, 0);
     weights /= (weights.x + weights.y + weights.z).xxx;
     return weights;
